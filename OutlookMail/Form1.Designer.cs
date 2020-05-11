6a8f22f4358ffe708fc75f9053c15b47c1f4293e
Tejuf <16520530@gm.uit.edu.vn>
2020-05-11 11:25:24 +0700

    Source Code

---
+++ OutlookMail/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace OutlookMail
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.txtSubject = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.Subject = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Body = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Sender = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DateTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnReceive = new System.Windows.Forms.Button();
+            this.txbFile = new System.Windows.Forms.TextBox();
+            this.btnAttachFile = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(315, 12);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(74, 23);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "To:";
+            // 
+            // txtTo
+            // 
+            this.txtTo.Location = new System.Drawing.Point(83, 49);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(334, 20);
+            this.txtTo.TabIndex = 2;
+            // 
+            // txtSubject
+            // 
+            this.txtSubject.Location = new System.Drawing.Point(84, 86);
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.Size = new System.Drawing.Size(334, 20);
+            this.txtSubject.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Subject:";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(84, 124);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtMessage.Size = new System.Drawing.Size(334, 70);
+            this.txtMessage.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Message:";
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Location = new System.Drawing.Point(423, 49);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(365, 360);
+            this.webBrowser.TabIndex = 7;
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Subject,
+            this.Body,
+            this.Sender,
+            this.DateTime});
+            this.dataGrid.Location = new System.Drawing.Point(36, 208);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.Size = new System.Drawing.Size(381, 230);
+            this.dataGrid.TabIndex = 8;
+            this.dataGrid.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGrid_CellClick);
+            // 
+            // Subject
+            // 
+            this.Subject.DataPropertyName = "Subject";
+            this.Subject.HeaderText = "Subject";
+            this.Subject.Name = "Subject";
+            // 
+            // Body
+            // 
+            this.Body.DataPropertyName = "Body";
+            this.Body.HeaderText = "Body";
+            this.Body.Name = "Body";
+            this.Body.Visible = false;
+            // 
+            // Sender
+            // 
+            this.Sender.DataPropertyName = "Sender";
+            this.Sender.HeaderText = "Sender";
+            this.Sender.Name = "Sender";
+            // 
+            // DateTime
+            // 
+            this.DateTime.DataPropertyName = "Date";
+            this.DateTime.HeaderText = "DateTime";
+            this.DateTime.Name = "DateTime";
+            // 
+            // btnReceive
+            // 
+            this.btnReceive.Location = new System.Drawing.Point(713, 415);
+            this.btnReceive.Name = "btnReceive";
+            this.btnReceive.Size = new System.Drawing.Size(75, 23);
+            this.btnReceive.TabIndex = 9;
+            this.btnReceive.Text = "Receive";
+            this.btnReceive.UseVisualStyleBackColor = true;
+            this.btnReceive.Click += new System.EventHandler(this.btnReceive_Click);
+            // 
+            // txbFile
+            // 
+            this.txbFile.Location = new System.Drawing.Point(12, 12);
+            this.txbFile.Name = "txbFile";
+            this.txbFile.ReadOnly = true;
+            this.txbFile.Size = new System.Drawing.Size(204, 20);
+            this.txbFile.TabIndex = 0;
+            // 
+            // btnAttachFile
+            // 
+            this.btnAttachFile.Location = new System.Drawing.Point(234, 12);
+            this.btnAttachFile.Name = "btnAttachFile";
+            this.btnAttachFile.Size = new System.Drawing.Size(75, 23);
+            this.btnAttachFile.TabIndex = 1;
+            this.btnAttachFile.Text = "Attach";
+            this.btnAttachFile.UseVisualStyleBackColor = true;
+            this.btnAttachFile.Click += new System.EventHandler(this.btnAttachFile_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(395, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Mail list";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnAttachFile);
+            this.Controls.Add(this.txbFile);
+            this.Controls.Add(this.btnReceive);
+            this.Controls.Add(this.dataGrid);
+            this.Controls.Add(this.webBrowser);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSubject);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSend);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        //private System.Windows.Forms.TextBox txbFile;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.TextBox txtSubject;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.Button btnReceive;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Subject;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Body;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Sender;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DateTime;
+        private System.Windows.Forms.TextBox txbFile;
+        private System.Windows.Forms.Button btnAttachFile;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ OutlookMail/Form1.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.Office.Interop.Outlook;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Attach = System.Net.Mail;
+using Exception = System.Exception;
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+namespace OutlookMail
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            Thread thread = new Thread(() =>
+            {
+                try
+                {
+
+
+                    Outlook._Application _app = new Outlook.Application();
+
+
+                    //attach = new Attach.Attachment(txbFile.Text);
+                    //if (attach != null)
+                    //{
+                    //mail.Attachments.Add(attach, Outlook.OlAttachmentType.olByValue);
+
+                    //}
+
+                    string[] emails = txtTo.Text.Split(' ');
+                    foreach (var item in emails)
+                    {
+                        var t = item.Trim();
+                        Outlook.MailItem mail = (Outlook.MailItem)_app.CreateItem(Outlook.OlItemType.olMailItem);
+                        mail.Subject = txtSubject.Text;
+                        mail.Body = txtMessage.Text;
+                        mail.Importance = Outlook.OlImportance.olImportanceNormal;
+                        if (!string.IsNullOrWhiteSpace(txbFile.Text))
+                        {
+                            FileInfo file = new FileInfo(txbFile.Text);
+                            mail.Attachments.Add(txbFile.Text);
+                        }
+                        if (t.LastIndexOf(',') == t.Length - 1)
+                        {
+                            var temp = item.Remove(item.Length - 1);
+                            mail.To = temp;
+                            ((Outlook._MailItem)mail).Send();
+                            continue;
+                        }
+                        mail.To = t;
+                        ((Outlook._MailItem)mail).Send();
+                    }
+                    MessageBox.Show("Your message has been successfully sent.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (System.Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            });
+            thread.Start();
+        }
+
+        DataTable dt;
+        private void btnReceive_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                Outlook._Application _app = new Outlook.Application();
+                Outlook._NameSpace _ns = _app.GetNamespace("MAPI");
+                Outlook.MAPIFolder inbox = _ns.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderInbox);
+                _ns.SendAndReceive(true);
+                dt = new DataTable("Inbox");
+                dt.Columns.Add("Subject", typeof(string));
+                dt.Columns.Add("Sender", typeof(string));
+                dt.Columns.Add("Body", typeof(string));
+                dt.Columns.Add("Date", typeof(string));
+                dataGrid.DataSource = dt;
+                foreach (Outlook.MailItem item in inbox.Items)
+                    dt.Rows.Add(new object[] { item.Subject, item.SenderName, item.HTMLBody, item.SentOn.ToLongDateString() + " " + item.SentOn.ToLongTimeString() });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dataGrid_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < dt.Rows.Count && e.RowIndex >= 0)
+                webBrowser.DocumentText = dt.Rows[e.RowIndex]["Body"].ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                StreamReader sr = new StreamReader(dialog.FileName);
+                string emails = "";
+                string email;
+                //var emails = new List<string>(1000);
+                while ((email = sr.ReadLine()) != null)
+                {
+                    emails += email + ", ";
+                }
+                //var str = "16520530@gm.uit.edu.vn, huypvn1998@gmail.com,";
+                txtTo.Text = emails.Remove(emails.Length - 2);
+                sr.Close();
+            }
+        }
+
+        private void btnAttachFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txbFile.Text = dialog.FileName;
+            }
+        }
+    }
+}
